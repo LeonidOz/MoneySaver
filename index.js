8b45f8d45678b4adf9874102641c5161de036aa0
LeonidOz <leonid@ozernyi.ru>
2025-06-15 11:31:44 +0300

    upd

--- index.js
+++ index.js
@@ -138,4 +138,4 @@ app.all('/', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3020;
-app.listen(PORT, () => console.log('🚀 Server running on http://localhost:'+PORT));
+app.listen(PORT, () => console.log('🚀 Server running on port: '+PORT));
